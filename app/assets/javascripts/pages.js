f73da4dbcc34e3b412ad5e65a2f646d7729f0964
matvacha <matvacha@yahoo.com>
2016-08-24 19:30:27 -0500

    update js

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,9 +1,17 @@
-var mapCallback = function(position) {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: position.coords.latitude, lng: position.coords.longitude},
-    zoom: 16
-    console.log(position.coords.latitude);
-  });
-
+if (navigator.geolocation) {
+  console.log('Geolocation is supported!');
+}
+else {
+  console.log('Geolocation is not supported for this Browser/OS version yet.');
+}
+window.onload = function() {
+  var mapCallback = function(position) {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: position.coords.latitude, lng: position.coords.longitude},
+      zoom: 16
+    });
+  };
+  console.log(position.coords.longitude);
+  console.log(position.coords.latitude);
   navigator.geolocation.getCurrentPosition(mapCallback);
 };
